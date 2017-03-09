ca0fe28a12265e78f8f98a952d819cebda8162fa
mclagett <mclagett@hotmail.com>
2017-03-09 14:00:22 -0500

    Cleaned up some unused code

--- ProjectTimelineGeneratorLib/Library1.fs
+++ ProjectTimelineGeneratorLib/Library1.fs
@@ -128,118 +128,116 @@ type TaskIterationLayoutEngine() =
         let rec addStoryTasksToDeveloperSchedule (developerLayout : developerLayout) 
                                                  (tasksToSchedule : task list) 
                                                  devTaskCommits us =
-                // iterate through unscheduled tasks and generate new developerTaskCommitment records for each one;
-                // on each iteration return hours left after subtracting this task's hours , as well as the list of
-                // developerTaskCommitments accumulated so far
-                let devTaskCommitmentLists =
-                    tasksToSchedule
-                    |> List.scan (fun (acc, devTaskCommitList) (t : task) -> 
-//                                        let retVal =
-                                        if ((acc >= 0.0) && ((t.remainingWork > 0.0) || ((t.remainingWork = 0.0) && (t.task.State = "Closed")))) then
-                                            let mutable overrideScheduled = false
-                                            let devTaskCommitment =
-                                                createTaskCommitment t developerLayout us
-                                            let availableHoursAgainstBudget =
-                                                match devTaskCommitment.taskState with
-                                                | "New" -> devTaskCommitment.remainingWork - t.hoursScheduledSoFar
-                                                | "Active" -> devTaskCommitment.remainingWork - t.hoursScheduledSoFar
-                                                | _ -> 0.0
+            // iterate through unscheduled tasks and generate new developerTaskCommitment records for each one;
+            // on each iteration return hours left after subtracting this task's hours , as well as the list of
+            // developerTaskCommitments accumulated so far
+            let devTaskCommitmentLists =
+                tasksToSchedule
+                |> List.scan (fun (acc, devTaskCommitList) (t : task) -> 
+                                    if ((acc >= 0.0) && ((t.remainingWork > 0.0) || ((t.remainingWork = 0.0) && (t.task.State = "Closed")))) then
+                                        let mutable overrideScheduled = false
+                                        let devTaskCommitment =
+                                            createTaskCommitment t developerLayout us
+                                        let availableHoursAgainstBudget =
+                                            match devTaskCommitment.taskState with
+                                            | "New" -> devTaskCommitment.remainingWork - t.hoursScheduledSoFar
+                                            | "Active" -> devTaskCommitment.remainingWork - t.hoursScheduledSoFar
+                                            | _ -> 0.0
 
-                                            // adjust status of scheduled task and calculate hours budgeted,
-                                            // depending on whether it was able to be completely scheduled
-                                            let mutable updatedBudget = acc - availableHoursAgainstBudget
-                                            let mutable hoursAgainstBudget =
-                                                if updatedBudget >= 0.0 then 
-                                                    t.scheduled <- true; t.hoursScheduledSoFar <- 0.0
-                                                    availableHoursAgainstBudget
-                                                else 
-                                                    t.scheduled <- false; t.hoursScheduledSoFar <- availableHoursAgainstBudget + updatedBudget;
-                                                    t.remainingWork <- t.remainingWork - availableHoursAgainstBudget;
-                                                    availableHoursAgainstBudget + updatedBudget
+                                        // adjust status of scheduled task and calculate hours budgeted,
+                                        // depending on whether it was able to be completely scheduled
+                                        let mutable updatedBudget = acc - availableHoursAgainstBudget
+                                        let mutable hoursAgainstBudget =
+                                            if updatedBudget >= 0.0 then 
+                                                t.scheduled <- true; t.hoursScheduledSoFar <- 0.0
+                                                availableHoursAgainstBudget
+                                            else 
+                                                t.scheduled <- false; t.hoursScheduledSoFar <- availableHoursAgainstBudget + updatedBudget;
+                                                t.remainingWork <- t.remainingWork - availableHoursAgainstBudget;
+                                                availableHoursAgainstBudget + updatedBudget
 
-                                            devTaskCommitment.hoursAgainstBudget <- hoursAgainstBudget
+                                        devTaskCommitment.hoursAgainstBudget <- hoursAgainstBudget
 
-                                            // if record is active, we may need to generate records for previous
-                                            // weeks in this iteration
-                                            let mutable updatedDevTaskCommitList = 
-                                                match devTaskCommitment.taskState with
-                                                | "Active" ->
-                                                    let iterationWeekList = [1 .. (currentWeekInIteration-1) ]
-                                                    let mutable updatedBudget2 = acc
-                                                    let listWithPriorIterations =
-                                                        iterationWeekList
-                                                        |> List.fold 
-                                                            (fun (acc2 : developerTaskCommitment list) 
-                                                                 (i : int) -> let overrideCompleted =
+                                        // if record is active, we may need to generate records for previous
+                                        // weeks in this iteration
+                                        let mutable updatedDevTaskCommitList = 
+                                            match devTaskCommitment.taskState with
+                                            | "Active" ->
+                                                let iterationWeekList = [1 .. (currentWeekInIteration-1) ]
+                                                let mutable updatedBudget2 = acc
+                                                let listWithPriorIterations =
+                                                    iterationWeekList
+                                                    |> List.fold 
+                                                        (fun (acc2 : developerTaskCommitment list) 
+                                                                (i : int) -> let overrideCompleted =
                                                                                 getInterimCompleted t currentIteration 
-                                                                              let currentTask = devTaskCommitment
-                                                                              if overrideCompleted.Length > 0 then
+                                                                             let currentTask = devTaskCommitment
+                                                                             if overrideCompleted.Length > 0 then
                                                                                 let priorTask = createTaskCommitment t developerLayout us
                                                                                 priorTask.isGeneratedPrecedingTask <- true
                                                                                 priorTask.completedWork <- overrideCompleted
-                                                                                                        |> List.last
-                                                                                                        |> (fun fc -> float (fc.postChangeValue.ToString()))
+                                                                                                            |> List.last
+                                                                                                            |> (fun fc -> float (fc.postChangeValue.ToString()))
                                                                                 priorTask.committedIterationWeek <- i
                                                                                 let overrideRemaining =
                                                                                     getInterimRemaining t currentIteration                            
                                                                                 if overrideRemaining.Length > 0 then
                                                                                     priorTask.remainingWork <- overrideRemaining
-                                                                                                            |> List.last
-                                                                                                            |> (fun fc -> float (fc.postChangeValue.ToString()))
-    //                                                                            devTaskCommitment.completedWork <- priorTask.remainingWork
+                                                                                                                |> List.last
+                                                                                                                |> (fun fc -> float (fc.postChangeValue.ToString()))
                                                                                 priorTask :: acc2
-                                                                              else 
+                                                                             else 
                                                                                 acc2
-                                                            ) 
-                                                            devTaskCommitList
-                                                    devTaskCommitment.projectedCompletedWork <- hoursAgainstBudget
-                                                    devTaskCommitment.projectedRemainingWork <- availableHoursAgainstBudget - hoursAgainstBudget
-                                                    listWithPriorIterations
-                                                | "Closed" ->
-                                                    devTaskCommitment.committedIteration <- getIterationFromPath devTaskCommitment.iterationCompleted
-                                                    devTaskCommitment.committedIterationWeek <- devTaskCommitment.iterationWeekCompleted
-                                                    devTaskCommitList
-                                                | "New" ->
-                                                    devTaskCommitment.projectedCompletedWork <- hoursAgainstBudget
-                                                    devTaskCommitment.projectedRemainingWork <- availableHoursAgainstBudget - hoursAgainstBudget
-                                                    devTaskCommitList
-                                                | _ ->
-                                                    devTaskCommitList
-                                            (updatedBudget, devTaskCommitment :: updatedDevTaskCommitList)
-                                        else
-                                            (acc, devTaskCommitList))
-                            (developerLayout.remainingHours, devTaskCommits)
+                                                        ) 
+                                                        devTaskCommitList
+                                                devTaskCommitment.projectedCompletedWork <- hoursAgainstBudget
+                                                devTaskCommitment.projectedRemainingWork <- availableHoursAgainstBudget - hoursAgainstBudget
+                                                listWithPriorIterations
+                                            | "Closed" ->
+                                                devTaskCommitment.committedIteration <- getIterationFromPath devTaskCommitment.iterationCompleted
+                                                devTaskCommitment.committedIterationWeek <- devTaskCommitment.iterationWeekCompleted
+                                                devTaskCommitList
+                                            | "New" ->
+                                                devTaskCommitment.projectedCompletedWork <- hoursAgainstBudget
+                                                devTaskCommitment.projectedRemainingWork <- availableHoursAgainstBudget - hoursAgainstBudget
+                                                devTaskCommitList
+                                            | _ ->
+                                                devTaskCommitList
+                                        (updatedBudget, devTaskCommitment :: updatedDevTaskCommitList)
+                                    else
+                                        (acc, devTaskCommitList))
+                        (developerLayout.remainingHours, devTaskCommits)
 
-                // the scan above has returned a list of intermediate states after each iteration
-                // return the first record with 0 or negative hours remaining; if there are none, take the last record
-                // selected record's list will be the list of tasks that had at least some time scheduled against time remaining
-                let remainingCommitmentLists =  
-                    devTaskCommitmentLists
-                    |> List.skipWhile(fun (hr,cl) -> hr >= 0.0 )
+            // the scan above has returned a list of intermediate states after each iteration
+            // return the first record with 0 or negative hours remaining; if there are none, take the last record
+            // selected record's list will be the list of tasks that had at least some time scheduled against time remaining
+            let remainingCommitmentLists =  
+                devTaskCommitmentLists
+                |> List.skipWhile(fun (hr,cl) -> hr >= 0.0 )
 
-                let devTaskCommitmentList =
-                    if remainingCommitmentLists = []then // all tasks fit with time left over so last record has correct task list
-                        devTaskCommitmentLists |> List.last 
+            let devTaskCommitmentList =
+                if remainingCommitmentLists = []then // all tasks fit with time left over so last record has correct task list
+                    devTaskCommitmentLists |> List.last 
 
-                    else  // some tasks that couldn't fit, so first remaining record has list 
-                        let (hr,cl) = remainingCommitmentLists |> List.head 
-                        if developerLayout.currentLayoutIterationWeek = 3 then
-                            let nextIteration = getNextIteration(developerLayout.currentLayoutIteration)
-                            developerLayout.currentLayoutIteration <- nextIteration
-                            developerLayout.currentLayoutIterationWeek <- 1
-                            cl.Head.isGeneratedPrecedingTask <- true
-                        else
-                            let nextIterationWeek = (developerLayout.currentLayoutIterationWeek + 1)
-                            developerLayout.currentLayoutIterationWeek <- nextIterationWeek
+                else  // some tasks that couldn't fit, so first remaining record has list 
+                    let (hr,cl) = remainingCommitmentLists |> List.head 
+                    if developerLayout.currentLayoutIterationWeek = 3 then
+                        let nextIteration = getNextIteration(developerLayout.currentLayoutIteration)
+                        developerLayout.currentLayoutIteration <- nextIteration
+                        developerLayout.currentLayoutIterationWeek <- 1
+                        cl.Head.isGeneratedPrecedingTask <- true
+                    else
+                        let nextIterationWeek = (developerLayout.currentLayoutIterationWeek + 1)
+                        developerLayout.currentLayoutIterationWeek <- nextIterationWeek
 
-                        developerLayout.remainingHours <- getDeveloperIterationWeek(developerLayout).totalHours
-                        addStoryTasksToDeveloperSchedule developerLayout 
-                                    (tasksToSchedule |> List.where(fun t -> t.scheduled = false))cl us
+                    developerLayout.remainingHours <- getDeveloperIterationWeek(developerLayout).totalHours
+                    addStoryTasksToDeveloperSchedule developerLayout 
+                                (tasksToSchedule |> List.where(fun t -> t.scheduled = false))cl us
     
-                // this will always be >= 0, because if it weren't, addStoryTasksToDeveloperSchedule
-                // woud be called recursively until the user story is completely laid out
-                developerLayout.remainingHours <- devTaskCommitmentList |> fst
-                devTaskCommitmentList
+            // this will always be >= 0, because if it weren't, addStoryTasksToDeveloperSchedule
+            // woud be called recursively until the user story is completely laid out
+            developerLayout.remainingHours <- devTaskCommitmentList |> fst
+            devTaskCommitmentList
 
         // schedule a user story's tasks for each assigned developer
         // used in a fold below to accumulate a list of task commitments for all a feature's user stories
